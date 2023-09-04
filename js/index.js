31ac6f91fa0b6a3651b78e376b5fcf82ee069b2d
EuheniHreben <tomasblack3@gmail.com>
2023-09-05 00:10:27 +0300

    fixes 04.09.23

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var blink = document.querySelector('#blink');
 function setRandomAnimationDuration() {
-  blink.style.animationDuration = Math.floor(Math.random() * 6 + 1) + "s";
+  blink.style.animationDuration = Math.floor(Math.random() * 4 + 1) + "s";
 }
  
 blink.addEventListener("animationiteration", setRandomAnimationDuration);
