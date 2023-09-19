739b69cd9936a61715d4c8203ae3e177a1a1c256
EuheniHreben <tomasblack3@gmail.com>
2023-09-20 01:19:09 +0300

    fixies 20.09.23

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var blink = document.querySelector('#blink');
+
 function setRandomAnimationDuration() {
   blink.style.animationDuration = Math.floor(Math.random() * 4 + 1) + "s";
 }
- 
+
 blink.addEventListener("animationiteration", setRandomAnimationDuration);
